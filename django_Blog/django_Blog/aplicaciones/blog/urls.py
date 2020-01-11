5d0fcc57aae3f1e28d5ed5c119d2eacfc80b1f03
remusezequiel <ezequielremus@gmail.com>
2020-01-11 16:30:42 -0300

    Modifico django_Blog

--- django_Blog/django_Blog/aplicaciones/blog/urls.py
+++ django_Blog/django_Blog/aplicaciones/blog/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
-from .views import index
+from .views import index,matematicas,programacion,fisica,quimica,posteo
 
 urlpatterns = [
-    path('',index, name='index'),
+    path('', index, name='index'),
+    path('matematicas/', matematicas, name='matematicas'),
+    path('programacion/', programacion, name='programacion'),
+    path('fisica/', fisica, name='fisica'),
+    path('quimica/', quimica, name='quimica'),
+    path('<slug:slug>/', posteo, name='posteo'),
 ]
--- django_Blog/django_Blog/aplicaciones/blog/views.py
+++ django_Blog/django_Blog/aplicaciones/blog/views.py
@@ -1,4 +1,46 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,get_object_or_404
+from .models import Post, Categoria
+from django.db.models import Q
 def index(request):
-    return render(request,'index.html')
+    #Si hay algo en la barra de busqueda se guarda aca
+    queryset = request.GET.get("buscar")
+    post = Post.objects.filter(estado__exact = True)
+    if queryset:
+        post = Post.objects.filter(
+            Q(titulo__icontains = queryset) |
+            Q(descripcion__icontains = queryset)
+        ).distinct()
+    return render(request,'index.html', {'post':post})
+
+def posteo(request,slug):
+    #post = Post.objects.get(slug=slug)
+    post = get_object_or_404(Post,slug=slug)
+    return render(request,'posts.html',{'post':post})
+
+def matematicas(request):
+    post = Post.objects.filter(
+        estado = True,
+        categoria = Categoria.objects.get(nombre__iexact='Matematicas')
+        )
+    return render(request,'matematicas.html', {'post':post})
+
+def programacion(request):
+    post = Post.objects.filter(
+        estado = True,
+        categoria = Categoria.objects.get(nombre__iexact='Programacion')
+        )
+    return render(request,'programacion.html', {'post':post})
+
+def fisica(request):
+    post = Post.objects.filter(
+        estado = True,
+        categoria = Categoria.objects.get(nombre__iexact='Fisica')
+        )
+    return render(request,'fisica.html', {'post':post})
+
+def quimica(request):
+    post = Post.objects.filter(
+        estado = True,
+        categoria = Categoria.objects.get(nombre__iexact='Quimica')
+        )
+    return render(request,'quimica.html', {'post':post})
